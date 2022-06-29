a83b941406f59193aec479c248b5d3edb3cb6812
c333721 <c333721>
2022-06-29 23:50:33 +0200

    start new cocktail project

---
+++ Cocktails/Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by c333721 on 29/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
