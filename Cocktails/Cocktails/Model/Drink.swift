03aa552c8f3d5235a8db70ae8aac8ebd5fb448cc
c333721 <c333721>
2022-06-30 22:47:18 +0200

    created drinks model

---
+++ Cocktails/Cocktails/Model/Drink.swift
@@ -0,0 +1,20 @@
+//
+//  Drink.swift
+//  Cocktails
+//
+//  Created by Pasquale on 30/06/22.
+//
+
+import Foundation
+
+struct Response: Codable {
+    let drinks: [Drink]
+}
+
+struct Drink: Codable {
+    let idDrink, strDrink, strCategory, strAlcoholic: String
+    let strInstructionsIT: String
+    let strDrinkThumb: String?
+    let strIngredient1, strIngredient2, strIngredient3: String?
+}
+
