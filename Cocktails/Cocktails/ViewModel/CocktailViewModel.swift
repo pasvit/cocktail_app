9cd86a006f5247ff02ed246cad8f2a119d293223
pasvit <pasquale.vitulli@gmail.com>
2022-07-01 12:27:37 +0200

    added cocktail item view+viewmodel

---
+++ Cocktails/Cocktails/ViewModel/CocktailViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  CocktailViewModel.swift
+//  Cocktails
+//
+//  Created by Pasquale on 30/06/22.
+//
+
+import UIKit
+
+class CocktailViewModel: CustomStringConvertible {
+    let id: String
+    let name: String
+    let category: String
+    let isAlcoholic: Bool
+    let instructionsIT: String?
+    var imageUrlString: String?
+    var image: UIImage? {
+        didSet {
+            self.bindImageToView()
+        }
+    }
+    
+    var description: String {
+        """
+        id:\(self.id)
+        name:\(self.name)
+        category:\(self.category)
+        isAlcoholic:\(self.isAlcoholic)
+        instructionsIT:\(self.instructionsIT ?? "empty")
+        imageUrlString:\(self.imageUrlString ?? "empty")\n\n
+        """
+    }
+    
+    // ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+    //    MARK: - Binding var
+    // \_____________________________________________________________________/
+    var bindImageToView : (() -> ()) = {}
+    
+    init(id: String, name: String, category: String, isAlcoholic: Bool, instructionsIT: String, imageUrlString: String) {
+        self.id = id
+        self.name = name
+        self.category = category
+        self.isAlcoholic = isAlcoholic
+        self.instructionsIT = instructionsIT
+        self.imageUrlString = imageUrlString
+        self.image = nil
+        fetchCocktailImage()
+    }
+    
+    // ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+    //    MARK: - Private Methods
+    // \_____________________________________________________________________/
+    private func fetchCocktailImage() {
+        guard let imageUrlString = self.imageUrlString else { return }
+        CocktailServiceFacade.fetchCocktailImage(with: imageUrlString) { result in
+            switch result {
+            case .success(let image):
+                self.image = image
+            case .failure(_):
+                break
+            }
+        }
+    }
+}
