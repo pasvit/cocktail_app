2926c25905b7da1bbcfafcf87ccbea5cb09ef05c
pasvit <pasquale.vitulli@gmail.com>
2022-07-01 23:31:16 +0200

    added API to retrieve a random cocktail

--- Cocktails/Cocktails/Service/CocktailServiceFacade.swift
+++ Cocktails/Cocktails/Service/CocktailServiceFacade.swift
@@ -70,6 +70,23 @@ class CocktailServiceFacade {
         }
     }
     
+    class func fetchRandomCocktail(completion: @escaping (Result<CocktailViewModel, DrinksError>) -> Void) {
+        DrinksService().fetchRandomDrink() { result in
+            switch result {
+            case .success(let cocktail):
+                guard let cocktail = cocktail else {
+                    completion(.failure(DrinksError.genericError))
+                    return 
+                }
+                let cocktailsVM = CocktailViewModel(id: cocktail.idDrink, name: cocktail.strDrink, category: cocktail.strCategory ?? "", glassType: cocktail.strGlass ?? "", alcoholic: cocktail.strAlcoholic ?? "", instructions: cocktail.strInstructions ?? "", ingredientsMeasures: getIngredientsMeasures(from: cocktail), imageUrlString: cocktail.strDrinkThumb ?? "")
+                completion(.success(cocktailsVM))
+                Log.info("fetchRandomDrink SUCCESS ->", cocktailsVM)
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
     class func fetchCocktailImage(with imageUrlString: String, completion: @escaping (Result<UIImage, DrinksError>) -> Void) {
         DrinksService().fetchDrinkImageData(with: imageUrlString) { result in
             switch result {
@@ -80,5 +97,47 @@ class CocktailServiceFacade {
                 break
             }
         }
+    }
+}
+
+extension CocktailServiceFacade {
+    static private func getIngredientsMeasures(from drink: Drink) -> [String: String] {
+        var ingredientsMeasures: [String: String] = [:]
+        
+        if let ingredient = drink.strIngredient1, let measure = drink.strMeasure1, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient2, let measure = drink.strMeasure2, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient3, let measure = drink.strMeasure3, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient4, let measure = drink.strMeasure4, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient5, let measure = drink.strMeasure5, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient6, let measure = drink.strMeasure6, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient7, let measure = drink.strMeasure7, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient8, let measure = drink.strMeasure8, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient9, let measure = drink.strMeasure9, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient10, let measure = drink.strMeasure10, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        if let ingredient = drink.strIngredient11, let measure = drink.strMeasure11, !ingredient.isEmpty, !measure.isEmpty {
+            ingredientsMeasures[ingredient] = measure
+        }
+        
+        return ingredientsMeasures
     }
 }
