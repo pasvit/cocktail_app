9569b33ba929c5342745c2e89b73319c1f42d55b
pasvit <pasquale.vitulli@gmail.com>
2022-07-01 23:32:09 +0200

    created view item to show an easter egg cocktail in list

---
+++ Cocktails/Cocktails/View/Cell/RandomCocktailTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  RandomCocktailTableViewCell.swift
+//  Cocktails
+//
+//  Created by c333721 on 01/07/22.
+//
+
+import UIKit
+
+class RandomCocktailTableViewCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    // ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+    //    MARK: - Public var
+    // \_____________________________________________________________________/
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        styleView()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    private func styleView() {
+        imgView.image = UIImage(named: "easterEgg_questionMark")
+        imgView.layer.cornerRadius = 15
+        
+        titleLabel.text = "No ideas?"
+        subtitleLabel.text = "Click here"
+    }
+}
