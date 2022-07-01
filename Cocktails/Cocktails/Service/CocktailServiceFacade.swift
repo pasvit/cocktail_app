d44219696be9add1776af66b9781273b25d80b11
pasvit <pasquale.vitulli@gmail.com>
2022-07-01 20:02:17 +0200

    update model+viewmodel to retrive more ingredients and informtions

--- Cocktails/Cocktails/Service/CocktailServiceFacade.swift
+++ Cocktails/Cocktails/Service/CocktailServiceFacade.swift
@@ -13,9 +13,9 @@ class CocktailServiceFacade {
         DrinksService().fetchDrinks(by: letter) { result in
             switch result {
             case .success(let drinks):
-                let cocktails = drinks.filter({ $0.strCategory == "Cocktail" })
+                let cocktails = drinks.filter({ $0.strCategory == "Cocktail" || $0.strCategory == "Ordinary Drink" })
                 let cocktailsVM = cocktails.map({ cocktail in
-                    CocktailViewModel(id: cocktail.idDrink, name: cocktail.strDrink, category: cocktail.strCategory ?? "", alcoholic: cocktail.strAlcoholic ?? "", instructionsIT: cocktail.strInstructionsIT ?? "", imageUrlString: cocktail.strDrinkThumb ?? "")
+                    CocktailViewModel(id: cocktail.idDrink, name: cocktail.strDrink, category: cocktail.strCategory ?? "", glassType: cocktail.strGlass ?? "", alcoholic: cocktail.strAlcoholic ?? "", instructions: cocktail.strInstructions ?? "", ingredientsMeasures: getIngredientsMeasures(from: cocktail), imageUrlString: cocktail.strDrinkThumb ?? "")
                 })
                 completion(.success(cocktailsVM))
                 Log.info("fetchCocktails SUCCESS ->", cocktailsVM)
@@ -23,13 +23,53 @@ class CocktailServiceFacade {
                 completion(.failure(error))
             }
         }
+        
+        func getIngredientsMeasures(from drink: Drink) -> [String: String] {
+            var ingredientsMeasures: [String: String] = [:]
+            
+            if let ingredient = drink.strIngredient1, let measure = drink.strMeasure1, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient2, let measure = drink.strMeasure2, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient3, let measure = drink.strMeasure3, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient4, let measure = drink.strMeasure4, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient5, let measure = drink.strMeasure5, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient6, let measure = drink.strMeasure6, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient7, let measure = drink.strMeasure7, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient8, let measure = drink.strMeasure8, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient9, let measure = drink.strMeasure9, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient10, let measure = drink.strMeasure10, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            if let ingredient = drink.strIngredient11, let measure = drink.strMeasure11, !ingredient.isEmpty, !measure.isEmpty {
+                ingredientsMeasures[ingredient] = measure
+            }
+            
+            return ingredientsMeasures
+        }
     }
     
     class func fetchCocktailImage(with imageUrlString: String, completion: @escaping (Result<UIImage, DrinksError>) -> Void) {
         DrinksService().fetchDrinkImageData(with: imageUrlString) { result in
             switch result {
             case .success(let imageData):
-                let cocktailImage = UIImage(data: imageData) ?? UIImage(named: "cocktail_image_default")!
+                let cocktailImage = UIImage(data: imageData) ?? UIImage()
                 completion(.success(cocktailImage))
             case .failure(_):
                 break
